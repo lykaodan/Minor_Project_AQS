4d3ab6d8511e5314b81c9505bf01aa0662d796ca
Ma. Angelica J. Odan <32080504+lykaodan@users.noreply.github.com>
2017-12-16 21:08:47 +0800

    creating database

--- config.php
+++ config.php
@@ -9,4 +9,82 @@
         //evaluate connection
         if($con->connect_errno)
             die("Could'nt Connect: " . $con-> connect_error);
-?>
+        
+		/*
+        //create database
+        $sql = "CREATE DATABASE minor_project";
+
+        if($con->query($sql))
+            echo "Database has been added";
+        else
+            echo "Error Creating the Database";
+        */
+		
+		
+		/*
+		//create table USER
+		$sql = "CREATE TABLE user( 
+            username VARCHAR(50) NOT NULL,
+            password VARCHAR(50) NOT NULL
+            )";
+			
+		if($con->query($sql))
+            echo "Table has been added";
+        else
+            echo "Error Creating the Table";	
+		*/
+
+        /*
+		//create table PRODUCT
+		 $sql = "CREATE TABLE product( 
+		 	product_id INT(100),
+            description VARCHAR(100),
+            qty int(100),
+		 	price int(100),
+		 	total int(100)
+            )";		
+			
+		if($con->query($sql))
+            echo "Table has been added";
+        else
+            echo "Error Creating the Table";
+		*/
+		
+		/*
+		//create table PRODUCT
+		 $sql = "CREATE TABLE purchase( 
+		 	p_product_id INT(100),
+            p_description VARCHAR(100),
+            p_qty int(100),
+		 	p_price int(100),
+		 	p_total int(100)
+            )";		
+			
+		if($con->query($sql))
+            echo "Table has been added";
+        else
+            echo "Error Creating the Table";
+		*/
+
+        /*
+		//create table transact
+		 $sql = "CREATE TABLE transact( 
+		 	all_total int(100),
+            downpayment int(100),
+		 	remaining_bal int(100)
+            )";		
+			
+		if($con->query($sql))
+            echo "Table has been added";
+        else
+            echo "Error Creating the Table";
+		*/
+
+        
+
+            /*<?php 
+ $date = date('Y-m-d H:i:s');
+ echo "Date: ".$date."<br>";
+?> */
+		
+    ?>
